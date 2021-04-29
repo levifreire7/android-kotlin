48fa672317d6bc49d779d40b960bc07d27e61882
Levi Freire <levifreire7@gmail.com>
2021-04-29 15:23:28 -0300

    Fragments do primeiro nível

---
+++ Exemplos/Navegacao/app/src/main/java/com/levifreire/navegacao/FirstLevelFragment.kt
@@ -0,0 +1,58 @@
+package com.levifreire.navegacao
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FirstLevelFragment : Fragment() {
+    private var navigationType: String? = null
+    private var actions = mutableMapOf<String, Class<*>>()
+    private var button: Button? = null
+    private var textView: TextView? = null
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        actions[getString(R.string.option_tab)] = TabsActivity::class.java
+//        actions[getString(R.string.option_bottom)] = BottomNavActivity::class.java
+//        actions[getString(R.string.option_pager)] = PagerActivity::class.java
+//
+//    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first_level, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button = view?.findViewById(R.id.button)
+        textView = view?.findViewById(R.id.textView)
+
+        navigationType = arguments?.getString(EXTRA_TYPE)
+        button?.setOnClickListener {
+            val key = navigationType
+            val clazz = actions[key]
+            startActivity(Intent(activity, clazz))
+        }
+        textView?.text = navigationType
+    }
+
+    companion object {
+        private const val EXTRA_TYPE = "tipoNavegacao"
+        fun newInstance(type: String): FirstLevelFragment {
+            val params = Bundle()
+            params.putString(EXTRA_TYPE, type)
+            val f = FirstLevelFragment()
+            f.arguments = params
+            return f
+        }
+    }
+}
--- Exemplos/Navegacao/app/src/main/java/com/levifreire/navegacao/MainActivity.kt
+++ Exemplos/Navegacao/app/src/main/java/com/levifreire/navegacao/MainActivity.kt
@@ -2,10 +2,69 @@ package com.levifreire.navegacao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toolbar: Toolbar
+    private lateinit var navigationView: NavigationView
+
+    private val drawerToggle: ActionBarDrawerToggle by lazy {
+        ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.app_name,
+            R.string.app_name
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawerLayout = findViewById(R.id.drawerLayout)
+        toolbar = findViewById(R.id.toolbar)
+        navigationView = findViewById(R.id.navigationView)
+
+        setSupportActionBar(toolbar)
+        drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            selectMenuOption(menuItem)
+            true
+        }
+
+        if (savedInstanceState == null) {
+            selectMenuOption(navigationView.menu.findItem(R.id.action_tab))
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun selectMenuOption(menuItem: MenuItem) {
+        menuItem.isChecked = true
+        drawerLayout.closeDrawers()
+        val title = menuItem.title.toString()
+        if (supportFragmentManager.findFragmentByTag(title) == null) {
+            val firstLevelFragment = FirstLevelFragment.newInstance(title)
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content, firstLevelFragment, title)
+                .commit()
+        }
     }
 }
