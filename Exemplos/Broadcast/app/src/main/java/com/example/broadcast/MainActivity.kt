11692b01c3e7c1ce1b1ca9916d2ef4c316a1ac66
levifreire7 <levifreire7@gmail.com>
2021-06-30 09:17:13 -0300

    Disparando  um broadcast receiver implicito

--- Exemplos/Broadcast/app/src/main/java/com/example/broadcast/MainActivity.kt
+++ Exemplos/Broadcast/app/src/main/java/com/example/broadcast/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.broadcast
 
+import android.content.ComponentName
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.broadcast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnSend.setOnClickListener {
+            sendImplicitBroadcast()
+        }
+
+    }
+
+    private fun sendImplicitBroadcast() {
+        val intent = Intent(ACTION_EVENT)
+        val matches = packageManager.queryBroadcastReceivers(intent, 0)
+        for (resolveInfo in matches) {
+            val explicit = Intent(intent)
+            val componentName = ComponentName(
+                resolveInfo.activityInfo.applicationInfo.packageName,
+                resolveInfo.activityInfo.name
+            )
+            explicit.component = componentName
+            sendBroadcast(explicit)
+        }
+    }
+
+    companion object {
+        private const val ACTION_EVENT = "com.example.broadcast.ACTION_EVENT"
     }
 }
