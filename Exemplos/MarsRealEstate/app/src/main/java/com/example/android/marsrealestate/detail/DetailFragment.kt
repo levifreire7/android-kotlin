78fe01b9ba36baecb00edb8cc56e1d30291b3194
levifreire7 <levifreire7@gmail.com>
2021-07-13 21:54:41 -0300

    Configurações iniciais do projeto

--- Exemplos/MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ Exemplos/MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -27,8 +27,10 @@ import com.example.android.marsrealestate.databinding.FragmentDetailBinding
  * This [Fragment] will show the detailed information about a selected piece of Mars real estate.
  */
 class DetailFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
 
         @Suppress("UNUSED_VARIABLE")
         val application = requireNotNull(activity).application
